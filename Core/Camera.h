b6ec346894aff87f17bd6b2e69a4f9d7b816f921
Marshall Feng <fengdier2@126.com>
2023-12-25 18:11:25 -0500

    Add a working camera

--- Core/Camera.h
+++ Core/Camera.h
@@ -1,18 +1,153 @@
 #pragma once
 
 #include <glm/glm.hpp>
+#include <glm/gtc/quaternion.hpp>
+#include <glm/gtx/quaternion.hpp>
 
 namespace VenusEngine
 {
-	struct Camera
+	/// \brief An eye that is viewing the scene.
+	class Camera
 	{
-		/// The location of this Camera in world coordinates.
-		glm::vec3 position;
-		/// The point in world coordinates that this Camera is focusing on.
-		glm::vec3 at;
-		/// The direction in world coordinates that this Camera considers to be up.
-		glm::vec3 up;
-		/// A projection matrix for this Camera.
-		glm::mat4 projection;
+	public:
+		/// \brief Constructs a new Camera.
+		///
+		/// \param eyePosition The location (in world coordinates) where the camera
+		///   is located.
+		/// \param localBackDirection A vector pointing backwards from the camera.
+		/// \param nearClipPlaneDistance The distance to the closest things the
+		///   camera can "see".
+		/// \param farClipPlaneDistance The distance to the farthest things the
+		///   camera can "see".
+		/// \param aspectRatio The window's width divided by height.
+		/// \param verticalFieldOfViewDegrees The angle determining how much the
+		///   camera "sees".
+		/// \post The camera's pose has been set and a projection matrix generated.
+		/// \post The three orientation vectors are orthonormal with each other.
+		/// \post All of the parameters have been copied into their respective data
+		///   members, and the local right and up have been calculated to be
+		///   orthonormal to the local back.
+		Camera(glm::vec3 const& position,
+			   float yaw,
+			   float pitch,
+			   float nearClipPlaneDistance,
+			   float farClipPlaneDistance,
+			   float aspectRatio,
+			   float verticalFieldOfViewDegrees)
+			: m_position(position),
+			  m_yaw(yaw),
+			  m_pitch(pitch),
+			  m_nearClipPlaneDistance(nearClipPlaneDistance),
+			  m_farClipPlaneDistance(farClipPlaneDistance),
+			  m_aspectRatio(aspectRatio),
+			  m_verticalFieldOfViewDegrees(verticalFieldOfViewDegrees)
+		{
+			updateCameraOrientation();
+		}
+
+		/// \brief Destructs a Camera.
+		~Camera()
+		{
+		}
+
+		/// \brief Sets the position (eye point) of the camera.
+		/// \param[in] position The new position of the camera.
+		/// \post The camera's location has been changed.
+		void setPosition(glm::vec3 const& position)
+		{
+			m_position = position;
+		}
+
+		/// \brief Moves the position (eye point) of the camera right or left.
+		/// \param[in] distance How far to move along the right vector.
+		/// \post The camera's location has been changed.
+		void moveRight(float distance)
+		{
+			m_position += m_rightDirection * distance;
+		}
+
+		/// \brief Moves the position (eye point) of the camera up or down.
+		/// \param[in] distance How far to move along the up vector.
+		/// \post The camera's location has been changed.
+		void moveUp(float distance)
+		{
+			m_position += m_upDirection * distance;
+		}
+
+		/// \brief Moves the position (eye point) of the camera back or forward.
+		/// \param[in] distance How far to move along the back vector.
+		/// \post The camera's location has been changed.
+		void moveFront(float distance)
+		{
+			m_position += m_frontDirection * distance;
+		}
+
+		/// \brief Gets the view matrix, recalculating it only if necessary.
+		/// \return A view matrix based on the camera's location and axis vectors.
+		glm::mat4 getViewMatrix()
+		{
+			return glm::lookAt(m_position, m_position + m_frontDirection, m_upDirection);
+		}
+
+		/// \brief Gets the projection matrix.
+		/// \return The projection matrix.
+		glm::mat4 getProjectionMatrix()
+		{
+			return glm::perspective(glm::radians(m_verticalFieldOfViewDegrees), m_aspectRatio, m_nearClipPlaneDistance, m_farClipPlaneDistance);
+		}
+
+		glm::mat4 getViewProjectionMatrix()
+		{
+			return getProjectionMatrix() * getViewMatrix();
+		}
+
+		/// \brief Resets the camera to its original pose.
+		/// \post The position (eye point) is the same as what had been specified in
+		///   the constructor.
+		/// \post The back vector is the same as what had been specified in the
+		///   constructor.
+		/// \post The up and right vectors are computed as they had been in the
+		///   constructor.
+		void resetPose()
+		{
+			m_position = glm::vec3(0.0f, 0.0f, 1.0f);
+			m_yaw   = 0.0f;
+			m_pitch = 0.0f;
+			updateCameraOrientation();
+		}
+
+	private:
+		void updateCameraOrientation()
+		{
+			glm::quat pitchQuat = glm::angleAxis(glm::radians(m_pitch), glm::vec3(1.0f, 0.0f, 0.0f));
+			glm::quat yawQuat   = glm::angleAxis(glm::radians(m_yaw), glm::vec3(0.0f, 1.0f, 0.0f));
+			glm::quat orientation = yawQuat * pitchQuat;
+			glm::mat4 rotationMatrix = glm::toMat4(orientation);
+			m_frontDirection = glm::normalize(glm::vec3(rotationMatrix * glm::vec4(0.0f, 0.0f, -1.0f, 0.0f)));
+			m_rightDirection = glm::normalize(glm::cross(m_frontDirection, glm::vec3(0.0f, 1.0f, 0.0f)));
+			m_upDirection    = glm::normalize(glm::cross(m_rightDirection, m_frontDirection));
+		}
+
+	private:
+		/// The location of the camera.
+		glm::vec3 m_position;
+
+		/// horizontal (in degree)
+		float m_yaw;
+		/// vertical (in degree)
+		float m_pitch;
+
+		float m_nearClipPlaneDistance;
+		float m_farClipPlaneDistance;
+		float m_aspectRatio;
+		/// fov (in degree)
+		float m_verticalFieldOfViewDegrees;
+
+		/// A vector pointing back from the camera.
+		glm::vec3 m_frontDirection;
+		/// A vector pointing right from the camera.
+		glm::vec3 m_rightDirection;
+		/// A vector pointing up from the camera.
+		glm::vec3 m_upDirection;
 	};
 }
--- Core/World.h
+++ Core/World.h
@@ -10,9 +10,11 @@ namespace VenusEngine
 	{
 	public:
 		World()
+			: m_camera(glm::vec3(0.0f, 0.0f, 15.0f), 0.0f, 0.0f, 0.01f, 40.0f, 1200.0f / 900.0f, 45.0f)
 		{
 			// 3 2D points, followed by 3 RGB colors
-			std::vector<float> triVertices{
+			std::vector<float> triVertices
+			{
 				0.0f, 5.0f, 0.0f,   // 3-d coordinates of first vertex(X, Y, Z)
 				1.0f, 0.0f, 0.0f,   // color of first vertex(R, G, B)
 				-5.0f, -5.0f, 0.0f, // 3-d coordinates of second vertex(X, Y, Z)
@@ -25,29 +27,15 @@ namespace VenusEngine
 			mesh_ptr->prepareVao();
 			m_scene.add("Triangle", mesh_ptr);
 			//////////////////////////////////////////////////////////////////////
-			// Define the projection, which will remain constant
-			constexpr float verticalFov = glm::radians(50.0f);
-			float aspectRatio = 1200.0f / 900;
-			// Near plane
-			float nearZ = 0.01f;
-			// Far plane
-			float farZ = 40.0f;
-			m_camera.projection = glm::perspective(verticalFov, aspectRatio, nearZ, farZ);
-			// Enable shader program so we can set uniforms
 			m_renderer.getShaderProgram().enable();
-			m_renderer.getShaderProgram().setUniformMatrix("uProjection", m_camera.projection);
+			m_renderer.getShaderProgram().setUniformMatrix("uProjection", m_camera.getProjectionMatrix());
 			m_renderer.getShaderProgram().disable();
-			// Define the camera's coordinate system
-			m_camera.position = glm::vec3(0, 0, 12.0f);
-			m_camera.at = glm::vec3(0, 0, 0);
-			m_camera.up = glm::vec3(0, 1, 0);
 		}
 
 		void draw()
 		{
 			m_renderer.clearBuffer();
-			glm::mat4 modelView = glm::lookAt(m_camera.position, m_camera.at, m_camera.up);
-			m_scene.draw(m_renderer.getShaderProgram(), modelView);
+			m_scene.draw(m_renderer.getShaderProgram(), m_camera.getViewMatrix());
 		}
 
 	private:
